e2c9e374555713e82a126f32972c13b51e3979df
JuanJustoMuller <juancitomuller@gmail.com>
2023-12-21 12:06:22 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Route, useLocation } from 'react-router-dom';
+import  Home from "./views/Home/Home";
+import Landing from './views/Landing/Landing';
+import Detail from './views/Details/Detail';
+import Form from './views/Form/Form';
+import NavBar from './components/NavBar/NavBar';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3001';
+
+function App() {
+
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      {location.pathname !== "/" && <NavBar />}
+      <Route exact path= "/" component={Landing}/>
+      <Route exact path= "/home" component={Home}/>
+      <Route path= "/detail/:id" component={Detail}/>
+      <Route exact path= "/create" component={Form}/>
+      
+    </div>
+  );
+}
+
+export default App;
